ccb99336f4b8cbf265094fbde9b6b76665afd1dd
jadc <chehimj@gmail.com>
2019-12-10 23:28:38 -0700

    done maybe

--- script.js
+++ script.js
@@ -1,6 +1,28 @@
-document.getElementsByTagName("button")[0].addEventListener("mouseup", async _ => {
-    let textarea = document.getElementsByTagName("textarea")[0].value;
-    let tts = await fetch("https://api.streamelements.com/kappa/v2/speech?voice=Brian&text=" + encodeURIComponent(textarea.trim()));
+let textarea = document.getElementsByTagName("textarea")[0];
+let length = 3000;
+
+// Functions
+let verifyLength = _ => {
+    let validLength = textarea.value.length > 0 && textarea.value.length <= length;
+    setButtonsState(validLength);
+
+    document.getElementsByClassName("count")[0].children[0].innerHTML = `${textarea.value.length}/${length}`;
+}
+
+let setButtonsState = state => {
+    let buttons = document.getElementsByClassName("toggle");
+    for(let i = 0; i < buttons.length; i++){
+        buttons[i].className = state ? "toggle" : "toggle disabled";
+    }
+}
+
+// Listeners
+
+let genBtn = document.getElementsByTagName("button")[0];
+genBtn.addEventListener("mouseup", async _ => {
+    if(genBtn.className === "toggle disabled") return;
+
+    let tts = await fetch("https://api.streamelements.com/kappa/v2/speech?voice=Brian&text=" + encodeURIComponent(textarea.value.trim()));
 
     if(tts.status != 200) {
         alert(await tts.text());
@@ -12,4 +34,26 @@ document.getElementsByTagName("button")[0].addEventListener("mouseup", async _ =
     audio.pause();
     audio.load();
     audio.play();
+});
+
+let dwnBtn = document.getElementsByTagName("button")[1];
+dwnBtn.addEventListener("mouseup", async _ => {
+    if(dwnBtn.className === "toggle disabled") return;
+    window.open("https://api.streamelements.com/kappa/v2/speech?voice=Brian&text=" + encodeURIComponent(textarea.value.trim()));
+});
+
+textarea.addEventListener("input", verifyLength);
+
+let limitBtn = document.getElementById("limit");
+let mode = 0;
+limitBtn.addEventListener("mouseup", _ => {
+    let modes = ["No Limit", "SE", "Cheer"];
+    mode = mode >= modes.length - 1 ? 0 : mode + 1;
+    limitBtn.innerHTML = modes[mode];
+
+    if(mode === 0) length = 3000;
+    if(mode === 1) length = 255;
+    if(mode === 2) length = 500;
+
+    return verifyLength();
 });
